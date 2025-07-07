9260f4b86f1f918a60428e836e60d2133b6b2898
baiman5678 <166015634+baiman5678@users.noreply.github.com>
2025-07-07 16:19:16 +0800

    backend upload

---
+++ backend/SkillExchangeAPI/SkillExchangeAPI/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿namespace SkillExchangeAPI.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+
+        public string Avatar { get; set; }
+        public string PasswordHash { get; set; } //存放加密後的密碼
+
+        public string Salt { get; set; } //加密用的鹽值
+        public int SkillPoints { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
